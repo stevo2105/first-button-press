b6e3430e3869114470027a4a25cc39c99bc4adba
Steven Schwartz <stevenschwartz43@gmail.com>
2025-05-12 23:29:33 -0400

    allow foreign server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      // Add your Whop App origin here
+      allowedOrigins: ["2ockbpmadrkpt4chjrc5.apps.whop.com"],
+    },
+  },
+};
 
 export default nextConfig;
